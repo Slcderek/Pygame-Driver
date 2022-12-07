580fc11ea343f23d986548ca4fd4bc40539c165f
slukay <80070385+slukay@users.noreply.github.com>
2022-12-06 20:40:53 -0700

    racecar go brrr

--- Py Driver Real/PyDriver.py
+++ Py Driver Real/PyDriver.py
@@ -10,33 +10,19 @@ racecar = pygame.transform.smoothscale(racecar, (80,80))
 racecar_rect = racecar.get_rect()
 racecar_rects = []
 
-friend_image = pygame.image.load("racecar.png")
-friend_image = pygame.transform.smoothscale(friend_image, (90,90))
-friend_rect = friend_image.get_rect()
-friend_rect.center = (400, 200)
+trophy_image = pygame.image.load("trophy.png")
+trophy_image = pygame.transform.smoothscale(trophy_image, (30,30))
+trophy_rect = trophy_image.get_rect()
+trophy_rect.center = (400, 200)
 pygame.key.set_repeat(1)
 while True: #this is the GAME loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-    # if event.type == pygame.KEYDOWN:
-    #     if event.key == pygame.K_LEFT:
-    #         racecar_rect.move_ip(-5,0)
-    #     racecar = pygame.transform.rotate(rat, 90)
-    #     if event.key == pygame.K_RIGHT:
-    #         rat_rect.move_ip(5, 0)
-    #     rat = pygame.transform.rotate(rat, 90)
-    #     if event.key == pygame.K_UP:
-    #         rat_rect.move_ip(0, -10)
-    #     if event.key == pygame.K_DOWN:
-    #         rat_rect.move_ip(0, 10)
-    #     if event.key == pygame.K_0:
-    #         pos = pygame.mouse.get_pos()
-    #         rat_rect.center = pos
         pos = pygame.mouse.get_pos()
         racecar_rect.center = pos
-    colliding = racecar_rect.colliderect(racecar_rect)
+    colliding = racecar_rect.colliderect(trophy_rect)
     print("colliding", colliding)
         #update positions
     # x = random.randint(0, screen_width)
@@ -52,7 +38,7 @@ while True: #this is the GAME loop
     # screen.blit(friend_image, friend_rect)
     # rat_rect.move_ip(1,0)
     if not colliding:
-        screen.blit(friend_image, racecar_rect)
+        screen.blit(trophy_image, trophy_rect)
     pygame.display.flip()
 
 
