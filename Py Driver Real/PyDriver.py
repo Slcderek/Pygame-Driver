4fbc1906f306d910fde0e9f2c8e350f257892ceb
slukay <80070385+slukay@users.noreply.github.com>
2022-12-06 21:27:34 -0700

    racecar go brrr

--- Py Driver Real/PyDriver.py
+++ Py Driver Real/PyDriver.py
@@ -2,9 +2,13 @@ import sys, pygame, random
 import pygame
 from DriverObjects import *
 pygame.init()
-screen_width = 800
-screen_height = 400
-screen = pygame.display.set_mode((screen_width, screen_height)) #create a screen
+
+map = pygame.image.load("background1.png")
+map_size = map.get_size()
+map_rect = map.get_rect()
+screen = pygame.display.set_mode(map_size) #create a screen
+map = map.convert_alpha()
+
 racecar = pygame.image.load("racecar.png")
 racecar = pygame.transform.smoothscale(racecar, (80,80))
 racecar_rect = racecar.get_rect()
@@ -14,6 +18,7 @@ trophy_image = pygame.image.load("trophy.png")
 trophy_image = pygame.transform.smoothscale(trophy_image, (30,30))
 trophy_rect = trophy_image.get_rect()
 trophy_rect.center = (400, 200)
+
 pygame.key.set_repeat(1)
 while True: #this is the GAME loop
     for event in pygame.event.get():
@@ -34,8 +39,9 @@ while True: #this is the GAME loop
     # rat_rect.center = (x,y)
     # rat_rect.move_ip(1,0)
     screen.fill( (30,90,100) ) #drawing everything
+    screen.blit(map, map_rect)
     screen.blit(racecar, racecar_rect)
-    # screen.blit(friend_image, friend_rect)
+
     # rat_rect.move_ip(1,0)
     if not colliding:
         screen.blit(trophy_image, trophy_rect)
